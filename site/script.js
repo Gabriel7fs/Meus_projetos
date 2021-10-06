85e015c1ce134d130c834b9977233d5503d7334a
Gabriel Furquim da Silva <88550409+Gabriel7fs@users.noreply.github.com>
2021-10-06 19:27:55 -0300

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,25 @@
+/*Botão lateral*/
+
+var hamburguer = document.querySelector(".hamburguer");
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+/*Produtos*/
+function carro1 (){
+    window.location.href = "carro1.html";
+}
+function carro2 (){
+    window.location.href = "carro2.html";
+}
+function carro3 (){
+    window.location.href = "carro3.html";
+}
+function carro4 (){
+    window.location.href = "carro4.html";
+}
+function carro5 (){
+    window.location.href = "carro5.html";
+}
+function carro6 (){
+    window.location.href = "carro6.html";
+}
